61976bc7fa7ffd45f626777698e2485b3d208628
Nicolas Pouzenc <n.pouzenc.pro@gmail.com>
2021-10-08 14:01:27 +0200

    Adding abstractions and packages references for Autofac and NLog

---
+++ IGMarkets.Debug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IGMarkets.Debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
